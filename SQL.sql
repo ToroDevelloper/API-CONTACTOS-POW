558bd145e41f75a306e2bab3f5f46df39fef158c
Angel <angeltoro736@gmail.com>
2025-10-31 11:33:19 -0500

    actualizacion db

--- SQL.sql
+++ SQL.sql
@@ -1,26 +1,26 @@
-/*
-SQLyog Ultimate v13.1.1 (64 bit)
-MySQL - 10.4.32-MariaDB : Database - contactos
-*********************************************************************
-*/
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.4.32-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.10.0.7000
+-- --------------------------------------------------------
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`contactos` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci */;
 
+
+-- Dumping database structure for contactos
+CREATE DATABASE IF NOT EXISTS `contactos` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci */;
 USE `contactos`;
 
-/*Table structure for table `contacto` */
-
-DROP TABLE IF EXISTS `contacto`;
-
-CREATE TABLE `contacto` (
+-- Dumping structure for table contactos.contacto
+CREATE TABLE IF NOT EXISTS `contacto` (
   `id_contacto` double NOT NULL AUTO_INCREMENT,
   `primer_nombre` varchar(50) NOT NULL,
   `segundo_nombre` varchar(50) DEFAULT NULL,
@@ -39,68 +39,39 @@ CREATE TABLE `contacto` (
   CONSTRAINT `contacto_ibfk_1` FOREIGN KEY (`id_genero`) REFERENCES `genero` (`id_genero`),
   CONSTRAINT `contacto_ibfk_2` FOREIGN KEY (`id_direccion`) REFERENCES `direccion` (`id_direccion`),
   CONSTRAINT `contacto_ibfk_3` FOREIGN KEY (`id_tipo_telefono`) REFERENCES `tipo_telefono` (`id_tipo_telefono`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
-/*Data for the table `contacto` */
+-- Data exporting was unselected.
 
-insert  into `contacto`(`id_contacto`,`primer_nombre`,`segundo_nombre`,`primer_apellido`,`segundo_apellido`,`id_genero`,`id_direccion`,`id_tipo_telefono`,`email`,`telefono`,`imagen`) values 
-(1,'JUAN','CAMILO','PEREZ','SOSSA',1,3,1,'JUANCAMILO@GMAIL.COM','12345665879',NULL),
-(2,'ANDRES','FERNANDO','LOPEZ','SUAREZ',1,4,2,'SUAREZ@GMAIL.COM','161616165',NULL);
-
-/*Table structure for table `direccion` */
-
-DROP TABLE IF EXISTS `direccion`;
-
-CREATE TABLE `direccion` (
+-- Dumping structure for table contactos.direccion
+CREATE TABLE IF NOT EXISTS `direccion` (
   `id_direccion` int(11) NOT NULL,
   `detalle_direccion` varchar(50) NOT NULL,
   PRIMARY KEY (`id_direccion`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
-/*Data for the table `direccion` */
+-- Data exporting was unselected.
 
-insert  into `direccion`(`id_direccion`,`detalle_direccion`) values 
-(1,'BARRIO JARDIN'),
-(2,'BARRIO LA ESMERALDA'),
-(3,'BARRIO LA RESERVA'),
-(4,'BARRIO LOS PRADOS'),
-(5,'BARRIO EL PEÑON');
-
-/*Table structure for table `genero` */
-
-DROP TABLE IF EXISTS `genero`;
-
-CREATE TABLE `genero` (
+-- Dumping structure for table contactos.genero
+CREATE TABLE IF NOT EXISTS `genero` (
   `id_genero` int(11) NOT NULL,
   `detalle_genero` varchar(50) NOT NULL,
   PRIMARY KEY (`id_genero`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
-/*Data for the table `genero` */
+-- Data exporting was unselected.
 
-insert  into `genero`(`id_genero`,`detalle_genero`) values 
-(1,'MASCULINO'),
-(2,'FEMENINO'),
-(3,'OTROS');
-
-/*Table structure for table `tipo_telefono` */
-
-DROP TABLE IF EXISTS `tipo_telefono`;
-
-CREATE TABLE `tipo_telefono` (
+-- Dumping structure for table contactos.tipo_telefono
+CREATE TABLE IF NOT EXISTS `tipo_telefono` (
   `id_tipo_telefono` int(11) NOT NULL,
   `detalle_tipo_telefono` varchar(50) NOT NULL,
   PRIMARY KEY (`id_tipo_telefono`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
-/*Data for the table `tipo_telefono` */
+-- Data exporting was unselected.
 
-insert  into `tipo_telefono`(`id_tipo_telefono`,`detalle_tipo_telefono`) values 
-(1,'HOGAR'),
-(2,'TRABAJO'),
-(3,'PRIVADO');
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
