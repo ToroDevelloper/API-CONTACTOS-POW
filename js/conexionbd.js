66c5f135b1adc5f25896bf42f07803d5960624e5
Angel <angeltoro736@gmail.com>
2025-11-02 17:25:08 -0500

    refactor: migrate to async/await and use connection pool for database queries

--- js/conexionbd.js
+++ js/conexionbd.js
@@ -1,81 +1,72 @@
-const mysql = require("mysql2")
-const express = require("express")
+const mysql = require('mysql2/promise')
+const express = require('express')
 const app = express()
 
-app.set("view engine", "ejs")
-app.use(express.static("public"))
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 
-const db = mysql.createConnection({
-    host: "localhost",
-    database: "contactos",
-    user: "root",
-    password: "",
-    port: 3306
+const pool = mysql.createPool({
+    host: 'localhost',
+    database: 'contactos',
+    user: 'root',
+    password: '',
+    port: 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
 })
 
-db.connect((err) => {
-    if (err) throw err
-    console.log("Conectado a la base de datos")
-})
-
-function getFormData(callback) {
-    db.query("SELECT * FROM genero", (err1, generos) => {
-        db.query("SELECT * FROM direccion", (err2, direcciones) => {
-            db.query("SELECT * FROM tipo_telefono", (err3, tipos) => {
-                callback({ generos, direcciones, tipos })
-            })
-        })
-    })
+async function getFormData() {
+    const [generos] = await pool.query('SELECT * FROM genero')
+    const [direcciones] = await pool.query('SELECT * FROM direccion')
+    const [tipos] = await pool.query('SELECT * FROM tipo_telefono')
+    return { generos, direcciones, tipos }
 }
 
-app.get("/", (req, res) => {
-    const sql = `
-        SELECT c.*, g.detalle_genero, d.detalle_direccion, t.detalle_tipo_telefono
-        FROM contacto c
-        INNER JOIN genero g ON c.id_genero = g.id_genero
-        INNER JOIN direccion d ON c.id_direccion = d.id_direccion
-        INNER JOIN tipo_telefono t ON c.id_tipo_telefono = t.id_tipo_telefono
-        ORDER BY c.id_contacto ASC
-    `
-
-    db.query(sql, (err, contactos) => {
-        if (err) throw err
-        res.render("index", { contactos })
-    })
+app.get('/', async (req, res) => {
+    try {
+        const sql = `
+            SELECT c.*, g.detalle_genero, d.detalle_direccion, t.detalle_tipo_telefono
+            FROM contacto c
+            INNER JOIN genero g ON c.id_genero = g.id_genero
+            INNER JOIN direccion d ON c.id_direccion = d.id_direccion
+            INNER JOIN tipo_telefono t ON c.id_tipo_telefono = t.id_tipo_telefono
+            ORDER BY c.id_contacto ASC
+        `
+        const [contactos] = await pool.query(sql)
+        res.render('index', { contactos })
+    } catch (err) {
+        console.error(err)
+        res.status(500).send('Error interno')
+    }
 })
 
-
-app.get("/crearcontacto", (req, res) => {
-    getFormData((data) => {
-        res.render("crearcontacto", data)
-    })
+app.get('/crearcontacto', async (req, res) => {
+    try {
+        const data = await getFormData()
+        res.render('crearcontacto', data)
+    } catch (err) {
+        console.error(err)
+        res.status(500).send('Error interno')
+    }
 })
 
-app.post("/contacto", (req, res) => {
-    const data = {}
-
-
-    Object.keys(req.body).forEach(key => {
-        data[key] = req.body[key] === '' ? null : req.body[key]
-    })
-
-    const sql = "INSERT INTO contacto SET ?"
-    db.query(sql, data, (err, result) => {
-        if (err) throw err
-        res.redirect("/")
-    })
+app.post('/contacto', async (req, res) => {
+    try {
+        const data = {}
+        Object.keys(req.body).forEach(k => data[k] = req.body[k] === '' ? null : req.body[k])
+        await pool.query('INSERT INTO contacto SET ?', data)
+        res.redirect('/')
+    } catch (err) {
+        console.error(err)
+        res.status(500).send('Error interno')
+    }
 })
 
-app.get("/estadisticas", (req, res) => {
-
-    const sqlTotal = "SELECT COUNT(*) as total FROM contacto"
-
-    db.query(sqlTotal, (err, totalResult) => {
-        if (err) throw err
-
-        const totalContactos = totalResult[0].total
-
+app.get('/estadisticas', async (req, res) => {
+    try {
+        const [[{ total }]] = await pool.query('SELECT COUNT(*) as total FROM contacto')
+        const totalContactos = total
         const sqlBarrios = `
             SELECT 
                 d.detalle_direccion as barrio,
@@ -86,18 +77,12 @@ app.get("/estadisticas", (req, res) => {
             GROUP BY d.id_direccion, d.detalle_direccion
             ORDER BY cantidad DESC
         `
-
-        db.query(sqlBarrios, [totalContactos], (err, barrios) => {
-            if (err) throw err
-
-            res.render("estadisticas", {
-                barrios,
-                totalContactos
-            })
-        })
-    })
+        const [barrios] = await pool.query(sqlBarrios, [totalContactos])
+        res.render('estadisticas', { barrios, totalContactos })
+    } catch (err) {
+        console.error(err)
+        res.status(500).send('Error interno')
+    }
 })
 
-app.listen(3000, () => {
-    console.log("Servidor en http://localhost:3000")
-})
+app.listen(3000, () => console.log('Servidor en http://localhost:3000'))
